1c543b0fc7453c67bb972fa59a50ce590138dc53
AppleBetas <applebetas@icloud.com>
2017-04-05 20:09:22 -0400

    Check config, use canvas size

--- bot.js
+++ bot.js
@@ -3,6 +3,12 @@ const config = require("./config");
 const client = new Discord.Client();
 let regex = /\([ ]*(\1[0-9]*)[ ]*[|.\-,][ ]*(\2[0-9]*)[ ]*\)+/ig;
 
+let canvasSize = config.canvasSize || 1000;
+if(!config.siteURL || !config.discordToken) {
+    console.log("Please set a site URL and Discord token in the config.");
+    process.exit();
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
@@ -12,6 +18,7 @@ client.on('message', msg => {
     if(!coordResults) return;
     let x = parseInt(coordResults[1]), y = parseInt(coordResults[2]);
     if(!x || !y || isNaN(x) || isNaN(y)) return;
+    if(x < 0 || y < 0 || x >= canvasSize || y >= canvasSize) return;
     let link = `${config.siteURL}/#x=${x}&y=${y}`;
     msg.channel.sendEmbed(new Discord.RichEmbed()
                               .setAuthor(msg.author.username, msg.author.avatarURL)
