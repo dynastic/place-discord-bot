8dc577a4cc5e4c889914cd3d0382873c933bf0d7
AppleBetas <applebetas@icloud.com>
2017-04-08 00:32:28 -0400

    Add thumbnail embeds

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require("discord.js");
 const config = require("./config");
+const request = require("request");
+const sharp = require("sharp");
+
 const client = new Discord.Client();
 let regex = /\([ ]*(\1-?[0-9]*)[ ]*[|.\-,][ ]*(\2-?[0-9]*)[ ]*\)+/ig;
 
@@ -9,9 +12,20 @@ if(!config.siteURL || !config.discordToken) {
     process.exit();
 }
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
-});
+function getCanvasImage(x, y) {
+    return new Promise((resolve, reject) => {
+        request({url: `${config.siteURL}/api/board-image`, method: "GET", encoding: null}, (error, response, body) => {
+            if(error) return reject(error);
+            if(!body) return reject();
+            let offset = ~(config.thumbnailSize / 2)
+            sharp(body)
+                .extract({ left: Math.min(canvasSize - config.thumbnailSize, Math.max(0, x + offset)), top: Math.min(canvasSize - config.thumbnailSize, Math.max(0, y + offset)), width: config.thumbnailSize, height: config.thumbnailSize })
+                .toBuffer().then(buffer => resolve(buffer)).catch(err => reject(err));
+        });
+    });
+}
+
+client.on('ready', () => console.log(`Logged in as ${client.user.username}!`));
 
 client.on('message', msg => {
     let coordResults = regex.exec(msg.cleanContent);
@@ -20,11 +34,17 @@ client.on('message', msg => {
     if(x === null || y === null || isNaN(x) || isNaN(y)) return;
     if(x < 0 || y < 0 || x >= canvasSize || y >= canvasSize) return;
     let link = `${config.siteURL}/#x=${x}&y=${y}`;
-    msg.channel.sendEmbed(new Discord.RichEmbed()
+    let embed = new Discord.RichEmbed()
                               .setAuthor(msg.author.username, msg.author.avatarURL)
                               .setTitle("Open in Place 2.0")
-                              .setDescription(`[Click here](${link}) to open the coordinates (**${x}**, **${y}**) in Place 2.0.`)
-    );
+                              .setDescription(`[Click here](${link}) to open the coordinates (**${x}**, **${y}**) in Place 2.0.`);
+    getCanvasImage(x, y).then(image => {
+        embed.setThumbnail("attachment://pixels.png");
+        msg.channel.send({embed: embed, file: { attachment: image, name: "pixels.png" }});
+    }).catch(err => {
+        console.error("Couldn't get canvas image: " + err);
+        msg.channel.sendEmbed(embed);
+    })
 });
 
 client.login(config.discordToken);
--- config.example.js
+++ config.example.js
@@ -1,5 +1,6 @@
 module.exports = {
     discordToken: "",
     siteURL: "https://place.dynastic.co",
-    canvasSize: 1000
+    canvasSize: 1000,
+    thumbnailSize: 75 // How many pixels the thumbnail should display
 }
