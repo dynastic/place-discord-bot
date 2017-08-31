0e53053a11e4dddef1ff8ea92b8e681ce4dc8e88
betas <applebetas@icloud.com>
2017-08-31 10:55:55 -0400

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     discordToken: "",
-    siteURL: "https://place.dynastic.co",
+    siteURL: "https://canvas.place",
     canvasSize: 1000,
     thumbnailSize: 75 // How many pixels the thumbnail should display
 }
